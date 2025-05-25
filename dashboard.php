0140f54789cdb7ad771f5c3a7b23cb95f388df2a
rklee13 <6075521+rklee13@users.noreply.github.com>
2025-05-24 20:24:29 -0500

    Add pie chart to dashboard
    Use HighCharts for pie chart
    Pie Chart shows breakdown of purchased order by status

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,9 @@ if (!isset($_SESSION['user']))
   header("location:login.php");
 
 $user = $_SESSION['user'];
+
+// Get the graph data - purchase order by status
+include('database/po_status_pie_graph.php');
 ?>
 
 <!DOCTYPE html>
@@ -23,13 +26,73 @@ $user = $_SESSION['user'];
     <?php include 'partials/app-sidebar.php' ?>
     <div class="dashboardContentContainer" id="dashboardContentContainer">
       <!-- Top Navigator bars -->
-      <?php include'partials/app-topnav.php' ?>
+      <?php include 'partials/app-topnav.php' ?>
       <div class="dashboardContent">
-        <div class="dashboardContentMain"></div>
+        <div class="dashboardContentMain">
+          <figure class="highcharts-figure">
+            <div id="pieChartContainer"></div>
+            <p class="highcharts-description" style="text-align: center">
+              Breakdown of all purchased orders by status.
+            </p>
+          </figure>
+        </div>
       </div>
     </div>
   </div>
 
   <script src="scripts/script.js"></script>
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+  <script src="https://code.highcharts.com/modules/exporting.js"></script>
+  <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+  <script src="https://code.highcharts.com/modules/export-data.js"></script>
+  <script>
+    Highcharts.chart('pieChartContainer', {
+      chart: {
+        type: 'pie'
+      },
+      title: {
+        text: 'Purchase Orders By Status'
+      },
+      tooltip: {
+        valueSuffix: ' orders'
+      },
+      subtitle: {
+        text:
+          'Source:<a href="https://www.mdpi.com/2072-6643/11/3/684/htm" target="_default">MDPI</a>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: [{
+            enabled: true,
+            distance: 20
+          }, {
+            enabled: true,
+            distance: -40,
+            format: '{point.percentage:.1f}%',
+            style: {
+              fontSize: '1.2em',
+              textOutline: 'none',
+              opacity: 0.7
+            },
+            filter: {
+              operator: '>',
+              property: 'percentage',
+              value: 10
+            }
+          }]
+        }
+      },
+      series: [
+        {
+          name: 'Status',
+          colorByPoint: true,
+          data: <?= json_encode($results) ?>
+        }
+      ]
+    });
+  </script>
 </body>
+
 </html>
---
+++ database/po_status_pie_graph.php
@@ -0,0 +1,53 @@
+<?php
+include('connection.php');
+
+$stmt = $connection->prepare("SELECT * FROM product_order WHERE 1");
+$stmt->execute();
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$response = $stmt->fetchAll();
+
+$statuses = [
+    'PENDING' => 0,
+    'INCOMPLETE' => 0,
+    'COMPLETE' => 0
+];
+foreach($response as $purchaseOrder) {
+    $statuses[$purchaseOrder['status']]++;
+}
+
+$results = [
+    [
+        'name'=> 'PENDING',
+        'y' => $statuses['PENDING'],
+    ],
+    [
+        'name'=> 'INCOMPLETE',
+        'y' => $statuses['INCOMPLETE'],
+    ],
+    [
+        'name'=> 'COMPLETE',
+        'y' => $statuses['COMPLETE'],
+    ]
+];
+
+// $statuses = ['PENDING','INCOMPLETE','COMPLETE'];
+// $results = [];
+// $total=0;
+// foreach($statuses as $status) {
+//     $stmt = $connection->prepare("SELECT count(*) FROM product_order WHERE status=?");
+//     $stmt->execute([$status]);
+//     $count=$stmt->fetchColumn();
+    
+//     $total=$total+$count;
+//     $results[] = [
+//         'name' => strtoupper($status),
+//         'y' => $count
+//     ];
+// }
+
+// echo '<pre>';
+// var_dump($results);
+// echo '</pre>';
+// die;
+
+?>
