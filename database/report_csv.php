7a679d2721f9b94009fac7bdad23b734f133df3e
rklee13 <6075521+rklee13@users.noreply.github.com>
2025-05-26 20:39:32 -0500

    Add order & deliveries to CSV export

--- database/report_csv.php
+++ database/report_csv.php
@@ -17,10 +17,28 @@ if ($type === 'products') {
     $select_query = "SELECT products.*,UserLoginInformation.first_name,UserLoginInformation.last_name FROM products INNER JOIN UserLoginInformation ON products.created_by=UserLoginInformation.id ORDER BY products.created_at DESC";
 } else if ($type === 'suppliers') {
     $select_query = "SELECT suppliers.*,UserLoginInformation.first_name,UserLoginInformation.last_name FROM suppliers INNER JOIN UserLoginInformation ON suppliers.created_by=UserLoginInformation.id ORDER BY suppliers.created_at DESC";
-} else if ($type === 'deliveries') {
-    $select_query = "SELECT * FROM product_order_history ORDER BY created_at DESC";
 } else if ($type === 'orders') {
-    $select_query = "SELECT * FROM product_order ORDER BY created_at DESC";
+    $select_query = "SELECT product_order.*,suppliers.supplier_name,products.product_name,UserLoginInformation.first_name,UserLoginInformation.last_name 
+        FROM product_order 
+        INNER JOIN products ON product_order.product=products.id
+        INNER JOIN suppliers ON product_order.supplier=suppliers.id
+        INNER JOIN UserLoginInformation ON product_order.created_by=UserLoginInformation.id
+        ORDER BY product_order.batch DESC";
+} else if ($type === 'deliveries') {
+    // $select_query = "SELECT product_order_history.*,suppliers.supplier_name,products.product_name 
+    //     FROM product_order_history 
+    //     INNER JOIN product_order ON product_order_history.product_order_id=product_order.id
+    //     INNER JOIN suppliers ON product_order.supplier=suppliers.id
+    //     INNER JOIN products ON product_order.product=products.id
+    //     ORDER BY product_order_history.date_received DESC";
+    $select_query = "SELECT product_order_history.date_received, product_order_history.quatity_received as quantity_received,product_order.batch,
+            UserLoginInformation.first_name,UserLoginInformation.last_name, products.product_name, suppliers.supplier_name 
+        FROM product_order_history,product_order,products,suppliers,UserLoginInformation
+        WHERE product_order_history.product_order_id=product_order.id 
+        AND product_order.created_by=UserLoginInformation.id 
+        AND product_order.supplier=suppliers.id 
+        AND product_order.product=products.id
+        ORDER BY product_order.batch DESC";
 }
 
 $stmt = $connection->prepare($select_query);
@@ -40,18 +58,40 @@ header("Content-Disposition: attachment; filename=\"$file_name\"");
 // Insert data into csv
 $obtain_header = true;
 $row_header = '';
-foreach ($dataRows as $row) {
+$add_line_break=false;
+$batchId='';
+foreach ($dataRows as $index=>$row) {
 
     // Clean up the data
     if ($type === 'products' || $type === 'suppliers') {
         $row['created_by'] = $row['first_name'] . ' ' . $row['last_name'];
-        $row['updated_at'] = date('M d,Y H:i:s A', strtotime($row['updated_at']));
-        $row['created_at'] = date('M d,Y H:i:s A', strtotime($row['created_at']));
+        $row['updated_at'] = date('Y-d-M H:i:s A', strtotime($row['updated_at']));
+        $row['created_at'] = date('Y-d-M H:i:s A', strtotime($row['created_at']));
         unset($row['first_name'], $row['last_name']);
-    } else if ($type === 'deliveries') {
-
     } else if ($type === 'orders') {
-
+        if ($batchId !== $row['batch']) {
+            $batchId = $row['batch'];
+            
+            // Don't add a line break for the first row
+            if ($index > 0) $add_line_break=true;
+        }
+        $row['created_by'] = $row['first_name'] . ' ' . $row['last_name'];
+        $row['product']=$row['product_name'];
+        $row['supplier']=$row['supplier_name'];
+        if (!$row['quantity_ordered']) $row['quantity_ordered']=0;
+        if (!$row['quantity_received']) $row['quantity_received']=0;
+        if (!$row['quantity_remaining']) $row['quantity_remaining']=0;
+        $row['updated_at'] = date('Y-d-M H:i:s A', strtotime($row['updated_at']));
+        $row['created_at'] = date('Y-d-M H:i:s A', strtotime($row['created_at']));
+        unset($row['first_name'], $row['last_name'], $row['product_name'],$row['supplier_name']);
+    } else if ($type === 'deliveries') {
+        if ($batchId !== $row['batch']) {
+            $batchId = $row['batch'];
+            
+            // Don't add a line break for the first row
+            if ($index > 0) $add_line_break=true;
+        }
+        $row['date_received'] = date('Y-d-M H:i:s A', strtotime($row['date_received']));
     }
 
     // Insert the header only once
@@ -68,6 +108,12 @@ foreach ($dataRows as $row) {
         if (strstr($str, '"'))
             $str = '"' . str_replace('"', '""', $str) . '"';
     });
+
+    // Add new line for additional specificity
+    if ($add_line_break) {
+        echo "\n";
+        $add_line_break=false;
+    }
 
     // Insert data
     //echo implode("\t", array_values($row)) . "\n";
--- database/report_pdf.php
+++ database/report_pdf.php
@@ -18,10 +18,22 @@ class IMSPDF extends FPDF
             $select_query = "SELECT products.*,UserLoginInformation.first_name,UserLoginInformation.last_name FROM products INNER JOIN UserLoginInformation ON products.created_by=UserLoginInformation.id ORDER BY products.created_at DESC";
         } else if ($type === 'suppliers') {
             $select_query = "SELECT suppliers.*,UserLoginInformation.first_name,UserLoginInformation.last_name FROM suppliers INNER JOIN UserLoginInformation ON suppliers.created_by=UserLoginInformation.id ORDER BY suppliers.created_at DESC";
-        } else if ($type === 'deliveries') {
-            $select_query = "SELECT * FROM product_order_history ORDER BY created_at DESC";
         } else if ($type === 'orders') {
-            $select_query = "SELECT * FROM product_order ORDER BY created_at DESC";
+            $select_query = "SELECT product_order.*,suppliers.supplier_name,products.product_name,UserLoginInformation.first_name,UserLoginInformation.last_name 
+                FROM product_order 
+                INNER JOIN products ON product_order.product=products.id 
+                INNER JOIN suppliers ON product_order.supplier=suppliers.id 
+                INNER JOIN UserLoginInformation ON product_order.created_by=UserLoginInformation.id 
+                ORDER BY product_order.batch DESC";
+        } else if ($type === 'deliveries') {
+            $select_query = "SELECT product_order_history.date_received, product_order_history.quatity_received as quantity_received,product_order.batch,
+                UserLoginInformation.first_name,UserLoginInformation.last_name, products.product_name, suppliers.supplier_name 
+                FROM product_order_history,product_order,products,suppliers,UserLoginInformation 
+                WHERE product_order_history.product_order_id=product_order.id 
+                AND product_order.created_by=UserLoginInformation.id 
+                AND product_order.supplier=suppliers.id 
+                AND product_order.product=products.id 
+                ORDER BY product_order.batch DESC";
         }
 
         include 'connection.php';
@@ -29,12 +41,6 @@ class IMSPDF extends FPDF
         $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
         return $stmt->fetchAll();
-        // Read file lines
-        // $lines = file($file);
-        // $data = array();
-        // foreach ($lines as $line)
-        //     $data[] = explode(';', trim($line));
-        // return $data;
     }
 
     function GetHeader($dataRows, $type)
@@ -45,12 +51,9 @@ class IMSPDF extends FPDF
             // Clean up the data
             if ($type === 'products' || $type === 'suppliers') {
                 unset($row_header['first_name'], $row_header['last_name']);
-                // if ($type === 'products') {
-                //     unset($row_header['image']);
-                // }
-            } else if ($type === 'deliveries') {
-
             } else if ($type === 'orders') {
+
+            } else if ($type === 'deliveries') {
 
             }
             return array_keys($row_header);
@@ -65,16 +68,15 @@ class IMSPDF extends FPDF
             foreach ($dataRows as $row) {
                 // Clean up the data
                 if ($type === 'products' || $type === 'suppliers') {
+                    $row['id'] = number_format($row['id']);
+                    $row['stock'] = number_format($row['stock']);
                     $row['created_by'] = $row['first_name'] . ' ' . $row['last_name'];
                     $row['updated_at'] = date('M d,Y H:i:s A', strtotime($row['updated_at']));
                     $row['created_at'] = date('M d,Y H:i:s A', strtotime($row['created_at']));
                     unset($row['first_name'], $row['last_name']);
-                    // if ($type === 'products') {
-                    //     unset($row['image']);
-                    // }
-                } else if ($type === 'deliveries') {
-
                 } else if ($type === 'orders') {
+
+                } else if ($type === 'deliveries') {
 
                 }
 
@@ -92,8 +94,8 @@ class IMSPDF extends FPDF
         return $data;
     }
 
-    // Colored table
-    function FancyTable($header, $data)
+    // Add table
+    function PopulateTable($header, $data)
     {
         // Colors, line width and bold font
         $this->SetFillColor(255, 0, 0);
@@ -112,10 +114,10 @@ class IMSPDF extends FPDF
         $this->SetFont('');
         // Data
         foreach ($data as $row) {
-            $this->Cell($w[0], 30, number_format($row[0]), 'LRBT', 0, 'C');
+            $this->Cell($w[0], 30, $row[0], 'LRBT', 0, 'C');
             $this->Cell($w[1], 30, $row[1], 'LRBT', 0, 'L');
-            
-            // Add a description that takes multiple lines/word wraps
+
+            // Add a description that takes multiple lines/word wraps. Note that h here is the height of the line and not the cell
             $current_y = $this->GetY();
             $current_x = $this->GetX();
             $this->MultiCell($w[2], 6, $row[2], 'LRT', 'L');
@@ -124,10 +126,10 @@ class IMSPDF extends FPDF
             // Add an image to the current x & y location of the cell
             $current_y = $this->GetY();
             $current_x = $this->GetX();
-            $image_name = $row[3] ? $this->Image('.././uploads/products/'.$row[3],$current_x,$current_y, 30, 25) : 'No image filed';
+            $image_name = $row[3] ? $this->Image('.././uploads/products/' . $row[3], $current_x, $current_y, 30, 25) : 'No image filed';
             $this->Cell($w[3], 30, $image_name, 'LRBT', 0, 'C'); // Image
 
-            $this->Cell($w[4], 30, number_format($row[4]), 'LRBT', 0, 'C');
+            $this->Cell($w[4], 30, $row[4], 'LRBT', 0, 'C');
             $this->Cell($w[5], 30, $row[5], 'LRBT', 0, 'C');
             $this->Cell($w[6], 30, $row[6], 'LRBT', 0, 'L');
             $this->Cell($w[7], 30, $row[7], 'LRBT', 0, 'L');
@@ -158,7 +160,7 @@ if (count($dataRows) > 0) {
 
     // Get data
     $data = $pdf->GetData($dataRows, $type);
-    
+
     $pdf->SetFont('Arial', '', 16);
     $pdf->AddPage();
     $pdf->Cell(110);
@@ -166,7 +168,7 @@ if (count($dataRows) > 0) {
     // Line break
     $pdf->Ln(20);
     $pdf->SetFont('Arial', '', 10);
-    $pdf->FancyTable($header, $data);
+    $pdf->PopulateTable($header, $data);
     $pdf->Output();
 }
 ?>
